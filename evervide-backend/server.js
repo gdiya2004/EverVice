69763a802d2b6bcf272550a790d1924e8ea20f39
Diya Gupta <192004gupta@gmail.com>
2026-04-19 23:27:59 +0530

    Port

--- evervide-backend/server.js
+++ evervide-backend/server.js
@@ -25,7 +25,7 @@ mongoose.connect(process.env.MONGO_URI)
 .then(()=>console.log("Mongo connected"))
 .catch(err=>console.log(err));
 
-const PORT=5000;
+const PORT=process.env.port || 5000;
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
 });
